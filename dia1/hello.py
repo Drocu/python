01dc82e32e0f93fb479b7c27584ee443bfc9fada
Rodrigo AY <drocu99@gmail.com>
2024-05-23 00:12:28 -0500

    Dia 1 de python

---
+++ dia1/hello.py
@@ -0,0 +1,15 @@
+#Practicando la configuracion del git-hub
+
+# Hola mundo
+print("Hola Mundo con Python")
+print('Hola Mundo')
+
+# consultar el tipo de dato
+print(type("Soy un dato str")) # Tipo 'str'
+print(type(5)) # Tipo 'int' 
+print(type(1.5)) # Tipo 'float'
+print(type(3 + 1j)) # Tipo 'complex'
+print(type(True)) # Tipo 'bool'
+print(type({'name':'Asabeneh'})) # Dictionary
+print(type({9.8, 3.14, 2.7})) #set
+print(type((9.8, 3.14, 2.7))) #Tuple
